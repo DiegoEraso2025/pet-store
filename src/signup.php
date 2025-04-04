01ec95970e35fb7c859ee5a413d546de8e9bcbfe
Diego Eraso <erasodiego5389@gmail.com>
2025-04-03 20:25:53 -0500

    Validate if email

--- src/signup.php
+++ src/signup.php
@@ -1,21 +1,52 @@
 <?php
 
- include('config/database.php');
+   include('config/database.php');
 
- $fname = $_POST['f_name'];
- $lname = $_POST['l_name'];
- $email = $_POST['e_mail'];
- $passw = $_POST['p_assw'];
+   $fname = $_POST['f_name'];
+   $lname = $_POST['l_name'];
+   $email = $_POST['e_mail'];
+   $passw = $_POST['p_assw'];
 
- $sql = "INSERT INTO users 
+   $sql_validate_email = "
+      select 
+          count(id) as total
+      from 
+        users
+      where
+        email= '$email'and 
+        status = true
+   ";
+   $ans = pg_query($conn, $sql_validate_email);
+
+   if($ans){ // ans == true 
+      $count = pg_fetch_assoc($ans);
+      if($row['total'] > 0){
+          echo "User already exits !!!"
+      }else{
+         $sql = "INSERT INTO users 
+                 (firstname, lastname, email, password)
+                 VALUES (' $fname','$lname','$email','$passw')
+         ";
+         $ans = pg_query($conn, $sql);
+         if($ans){
+             echo "User has been created successfully";
+         }else{
+             echo "Error";
+         }
+
+      }
+}else{
+         }
+
+       $sql = "INSERT INTO users 
         (firstname, lastname, email, password)
         VALUES (' $fname','$lname','$email','$passw')
- ";
- $ans = pg_query($conn, $sql);
- if($ans){
-    echo "User has been created successfully";
- }else{
-    echo "Error";
- }
+       ";
+       $ans = pg_query($conn, $sql);
+       if($ans){
+       echo "User has been created successfully";
+       }else{
+      echo "Error";
+   }
 
 ?>
